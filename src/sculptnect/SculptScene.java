8c815c62daaa9e4f15ea4e6c021b924ef11b91a5
Emil Arfvidsson <emil@arfvidsson.com>
2012-09-21 22:07:55 +0200

    Replace some hardcoded values with constants.

--- src/sculptnect/SculptScene.java
+++ src/sculptnect/SculptScene.java
@@ -11,9 +11,9 @@ import javax.media.opengl.glu.gl2.GLUgl2;
 import javax.vecmath.Point3i;
 import javax.vecmath.Vector2f;
 
+import joystick.JoystickListener;
 import shape.CubeGenerator;
 import shape.SphereGenerator;
-import joystick.JoystickListener;
 
 public class SculptScene implements GLEventListener, JoystickListener {
 	private final int VOXEL_GRID_SIZE = 200;
@@ -21,27 +21,29 @@ public class SculptScene implements GLEventListener, JoystickListener {
 	private final short KINECT_FAR_THRESHOLD = KinectUtils.metersToRawDepth(1.4f);
 	private final float KINECT_DEPTH_FACTOR = 500.0f;
 
+	private static final int DEPTH_WIDTH = 640;
+	private static final int DEPTH_HEIGHT = 480;
+
 	private final float JOYSTICK_ROTATION_SENSITIVITY = 3.0f;
 
 	private static final Vector2f INITIAL_ROTATION = new Vector2f((float) Math.PI * 0.0f, (float) Math.PI * 1.0f);
 
-	private VoxelGrid _grid;
+	private VoxelGrid grid;
 
-	private Vector2f _rotation = new Vector2f(INITIAL_ROTATION);
+	private Vector2f rotation = new Vector2f(INITIAL_ROTATION);
 	private Vector2f rotationSpeed = new Vector2f();
 
-	private float filteredDepth[][] = new float[640][480];
-	private float depth[][] = new float[640][480];
-	private float depthNormals[][][] = new float[640][480][3];
+	private float filteredDepth[][] = new float[DEPTH_WIDTH][DEPTH_HEIGHT];
+	private float depth[][] = new float[DEPTH_WIDTH][DEPTH_HEIGHT];
+	private float depthNormals[][][] = new float[DEPTH_WIDTH][DEPTH_HEIGHT][3];
 
 	private float modelRotationX = 0.0f;
 	private float modelRotationY = 0.0f;
 
 	private float modelRotationSpeedX = 0.0f;
 	private float modelRotationSpeedY = 0.0f;
-	
-	private boolean turningMode;
 
+	private boolean turningMode;
 
 	@Override
 	public void init(GLAutoDrawable drawable) {
@@ -67,7 +69,7 @@ public class SculptScene implements GLEventListener, JoystickListener {
 		float ambientColor[] = { 0.5f, 0.5f, 0.5f, 1.0f };
 		float light = 0.0055f;
 		float lightColor1[] = { light, light, light, 1.0f };
-		float specularColor[] = {1.0f, 1.0f, 1.0f, 1.0f};
+		float specularColor[] = { 1.0f, 1.0f, 1.0f, 1.0f };
 		float lightPos1[] = { 100000.0f, 100000.0f, 100000.0f, 1.0f };
 
 		// Set light color and position
@@ -79,15 +81,15 @@ public class SculptScene implements GLEventListener, JoystickListener {
 
 		// Create voxel grid
 		int size = VOXEL_GRID_SIZE;
-		_grid = new VoxelGrid(gl, size);
-		
+		grid = new VoxelGrid(gl, size);
+
 		resetModel();
 	}
-	
+
 	public void resetModel() {
 		// Add sphere shape to voxel grid
 		CubeGenerator generator = new CubeGenerator(VoxelGrid.VOXEL_GRID_CLAY, new Point3i(VOXEL_GRID_SIZE / 2, VOXEL_GRID_SIZE / 2, VOXEL_GRID_SIZE / 2), VOXEL_GRID_SIZE / 2 - 2);
-		_grid.insertShape(generator);
+		grid.insertShape(generator);
 	}
 
 	@Override
@@ -109,13 +111,13 @@ public class SculptScene implements GLEventListener, JoystickListener {
 
 	@Override
 	public void display(GLAutoDrawable drawable) {
-		_rotation.add(rotationSpeed);
+		rotation.add(rotationSpeed);
 
 		modelRotationX = (modelRotationX + modelRotationSpeedX + 360.0f) % 360.0f;
 		modelRotationY = (modelRotationY + modelRotationSpeedY + 360.0f) % 360.0f;
 
 		GL2 gl = (GL2) drawable.getGL();
-		
+
 		removeRandomSphere();
 
 		// Set default vertex color
@@ -130,11 +132,11 @@ public class SculptScene implements GLEventListener, JoystickListener {
 		gl.glTranslatef(0.0f, 0.0f, -3.5f);
 
 		// Scale down the grid to fit on screen
-		gl.glScalef(2.0f / _grid.width, 2.0f / _grid.height, 2.0f / _grid.depth);
+		gl.glScalef(2.0f / grid.width, 2.0f / grid.height, 2.0f / grid.depth);
 
 		// Rotate around x and y axes
-		gl.glRotatef(_rotation.x * 57.2957795f, 1.0f, 0.0f, 0.0f);
-		gl.glRotatef(_rotation.y * 57.2957795f, 0.0f, 1.0f, 0.0f);
+		gl.glRotatef(rotation.x * 57.2957795f, 1.0f, 0.0f, 0.0f);
+		gl.glRotatef(rotation.y * 57.2957795f, 0.0f, 1.0f, 0.0f);
 
 		// Draw voxel grid
 		gl.glPointSize(6.0f);
@@ -143,7 +145,7 @@ public class SculptScene implements GLEventListener, JoystickListener {
 
 		gl.glRotatef(modelRotationY, 0.0f, -1.0f, 0.0f);
 		gl.glTranslatef(-VOXEL_GRID_SIZE * 0.5f, -VOXEL_GRID_SIZE * 0.5f, -VOXEL_GRID_SIZE * 0.5f);
-		_grid.draw(gl);
+		grid.draw(gl);
 		gl.glPopMatrix();
 
 		// Disable lighting to draw axis lines
@@ -155,11 +157,11 @@ public class SculptScene implements GLEventListener, JoystickListener {
 		gl.glTranslatef(-320.0f, -240.0f, -KINECT_DEPTH_FACTOR * 0.5f);
 		gl.glBegin(GL.GL_POINTS);
 		gl.glColor4f(0.5f, 0.5f, 0.5f, 0.5f);
-		for (int x = 0; x < 640; ++x) {
-			for (int y = 0; y < 480; ++y) {
+		for (int x = 0; x < DEPTH_WIDTH; ++x) {
+			for (int y = 0; y < DEPTH_HEIGHT; ++y) {
 				if (depth[x][y] > 0.0f) {
-					//gl.glNormal3fv(depthNormals[x][y], 0);
-					gl.glVertex3f(x, 480 - y, depth[x][y] * KINECT_DEPTH_FACTOR);
+					// gl.glNormal3fv(depthNormals[x][y], 0);
+					gl.glVertex3f(x, DEPTH_HEIGHT - y, depth[x][y] * KINECT_DEPTH_FACTOR);
 				}
 			}
 		}
@@ -172,17 +174,17 @@ public class SculptScene implements GLEventListener, JoystickListener {
 		// X
 		gl.glColor3f(1.0f, 0.0f, 0.0f);
 		gl.glVertex3f(0.0f, 0.0f, 0.0f);
-		gl.glVertex3f(_grid.width, 0.0f, 0.0f);
+		gl.glVertex3f(grid.width, 0.0f, 0.0f);
 
 		// Y
 		gl.glColor3f(0.0f, 1.0f, 0.0f);
 		gl.glVertex3f(0.0f, 0.0f, 0.0f);
-		gl.glVertex3f(0.0f, _grid.height, 0.0f);
+		gl.glVertex3f(0.0f, grid.height, 0.0f);
 
 		// Z
 		gl.glColor3f(0.0f, 0.0f, 1.0f);
 		gl.glVertex3f(0.0f, 0.0f, 0.0f);
-		gl.glVertex3f(0.0f, 0.0f, _grid.depth);
+		gl.glVertex3f(0.0f, 0.0f, grid.depth);
 		gl.glEnd();
 
 		// Enable light again
@@ -190,29 +192,30 @@ public class SculptScene implements GLEventListener, JoystickListener {
 	}
 
 	public void mouseDragged(int prevX, int prevY, int x, int y) {
-		_rotation.x += (y - prevY) / 100.0;
-		_rotation.y += (x - prevX) / 100.0;
+		rotation.x += (y - prevY) / 100.0;
+		rotation.y += (x - prevX) / 100.0;
 
-		_rotation.x = Math.max((float) -Math.PI / 2.0f, _rotation.x);
-		_rotation.x = Math.min((float) Math.PI / 2.0f, _rotation.x);
+		rotation.x = Math.max((float) -Math.PI / 2.0f, rotation.x);
+		rotation.x = Math.min((float) Math.PI / 2.0f, rotation.x);
 	}
 
 	public void removeRandomSphere() {
 		int size = (int) (Math.random() * 30);
-		Point3i center = new Point3i((int) (Math.random() * _grid.width), (int) (Math.random() * _grid.height), (int) (Math.random() * _grid.depth));
+		Point3i center = new Point3i((int) (Math.random() * grid.width), (int) (Math.random() * grid.height), (int) (Math.random() * grid.depth));
 
 		// Add sphere shape to voxel grid
 		SphereGenerator sphereGenerator = new SphereGenerator(VoxelGrid.VOXEL_GRID_AIR, center, size);
-		_grid.insertShape(sphereGenerator);
+		grid.insertShape(sphereGenerator);
 	}
 
 	public void updateKinect(ByteBuffer depthBuffer) {
 		depthBuffer.rewind();
 		depthBuffer.order(ByteOrder.LITTLE_ENDIAN);
 
-		// Retrieve Kinect depth data within the near and far threshold to a depth array
-		for (int y = 0; y < 480; ++y) {
-			for (int x = 0; x < 640; ++x) {
+		// Retrieve Kinect depth data within the near and far threshold to a
+		// depth array
+		for (int y = 0; y < DEPTH_HEIGHT; ++y) {
+			for (int x = 0; x < DEPTH_WIDTH; ++x) {
 				short rawDepth = depthBuffer.getShort();
 				if (rawDepth < KINECT_NEAR_THRESHOLD || rawDepth > KINECT_FAR_THRESHOLD) {
 					depth[x][y] = 0.0f;
@@ -223,10 +226,13 @@ public class SculptScene implements GLEventListener, JoystickListener {
 		}
 
 		final int radius = 4;
-		int bounds[] = { 320 - (int) (Math.sqrt(3) * VOXEL_GRID_SIZE * 0.5f), 320 + (int) (Math.sqrt(3) * VOXEL_GRID_SIZE * 0.5f), 240 - (int) (Math.sqrt(3) * VOXEL_GRID_SIZE * 0.5f), 240 + (int) (Math.sqrt(3) * VOXEL_GRID_SIZE * 0.5f) };
+		int bounds[] = { DEPTH_WIDTH - (int) (Math.sqrt(3) * VOXEL_GRID_SIZE * 0.5f), //
+				DEPTH_WIDTH + (int) (Math.sqrt(3) * VOXEL_GRID_SIZE * 0.5f), //
+				DEPTH_HEIGHT - (int) (Math.sqrt(3) * VOXEL_GRID_SIZE * 0.5f), //
+				DEPTH_HEIGHT + (int) (Math.sqrt(3) * VOXEL_GRID_SIZE * 0.5f) };
 
-		for (int x = radius; x < 640 - radius; ++x) {
-			for (int y = radius; y < 480 - radius; ++y) {
+		for (int x = radius; x < DEPTH_WIDTH - radius; ++x) {
+			for (int y = radius; y < DEPTH_HEIGHT - radius; ++y) {
 				// Optimization, ignore points too far from the model
 				if (x < bounds[0] || x > bounds[1] || y < bounds[2] || y > bounds[3]) {
 					continue;
@@ -242,11 +248,12 @@ public class SculptScene implements GLEventListener, JoystickListener {
 				filteredDepth[x][y] = total / ((radius * 2 + 1) * (radius * 2 + 1));
 
 				for (int i = 0; i < 30; ++i) {
-					float xOrig = x - 320;
-					float yOrig = (479 - y) - 240;
+					float xOrig = x - DEPTH_WIDTH / 2;
+					float yOrig = (DEPTH_HEIGHT - 1 - y) - DEPTH_HEIGHT / 2;
 					float zOrig = filteredDepth[x][y] * KINECT_DEPTH_FACTOR - KINECT_DEPTH_FACTOR * 0.5f - i;
 
-					// Rotate the points the same amount that the model is rotated
+					// Rotate the points the same amount that the model is
+					// rotated
 					float xVal = (float) (xOrig * SculptMath.cos(modelRotationY) + yOrig * SculptMath.sin(modelRotationY) * SculptMath.sin(modelRotationX) + zOrig * SculptMath.sin(modelRotationY) * SculptMath.cos(modelRotationX));
 					float yVal = (float) (yOrig * SculptMath.cos(modelRotationX) - zOrig * SculptMath.sin(modelRotationX));
 					float zVal = (float) (-xOrig * SculptMath.sin(modelRotationY) + yOrig * SculptMath.cos(modelRotationY) * SculptMath.sin(modelRotationX) + zOrig * SculptMath.cos(modelRotationY) * SculptMath.cos(modelRotationX));
@@ -255,9 +262,10 @@ public class SculptScene implements GLEventListener, JoystickListener {
 					int yPos = (int) (yVal + VOXEL_GRID_SIZE / 2);
 					int zPos = (int) (zVal + VOXEL_GRID_SIZE / 2);
 
-					// Check whether the point is within the bounding box of the model
+					// Check whether the point is within the bounding box of the
+					// model
 					if (zPos >= 0 && zPos < VOXEL_GRID_SIZE && xPos >= 0 && xPos < VOXEL_GRID_SIZE && yPos >= 0 && yPos < VOXEL_GRID_SIZE) {
-						_grid.setVoxel(xPos, yPos, zPos, VoxelGrid.VOXEL_GRID_AIR);
+						grid.setVoxel(xPos, yPos, zPos, VoxelGrid.VOXEL_GRID_AIR);
 					} else {
 						break;
 					}
@@ -265,9 +273,10 @@ public class SculptScene implements GLEventListener, JoystickListener {
 			}
 		}
 
-		// Estimate the normals of the Kinect point cloud from neighboring points
-		for (int x = 1; x < 639; ++x) {
-			for (int y = 1; y < 479; ++y) {
+		// Estimate the normals of the Kinect point cloud from neighboring
+		// points
+		for (int x = 1; x < DEPTH_WIDTH - 1; ++x) {
+			for (int y = 1; y < DEPTH_HEIGHT - 1; ++y) {
 				depthNormals[x][y][0] = depth[x + 1][y] - depth[x - 1][y];
 				depthNormals[x][y][1] = depth[x][y + 1] - depth[x][y - 1];
 				depthNormals[x][y][2] = 0.01f;
@@ -295,14 +304,14 @@ public class SculptScene implements GLEventListener, JoystickListener {
 			modelRotationX = 0;
 			modelRotationY = 0;
 		} else if (button.equals("11")) {
-			_rotation.set(INITIAL_ROTATION);
-		} else if (button.equals("7")) { //left
+			rotation.set(INITIAL_ROTATION);
+		} else if (button.equals("7")) { // left
 			modelRotationSpeedY = value ? 2.0f : 0.0f;
-		} else if (button.equals("5")) { //right
+		} else if (button.equals("5")) { // right
 			modelRotationSpeedY = value ? -2.0f : 0.0f;
-		} else if (button.equals("4")) { //up
+		} else if (button.equals("4")) { // up
 			modelRotationSpeedX = value ? 2.0f : 0.0f;
-		} else if (button.equals("6")) { //down
+		} else if (button.equals("6")) { // down
 			modelRotationSpeedX = value ? -2.0f : 0.0f;
 		} else if (value && button.equals("3")) {
 			resetModel();
